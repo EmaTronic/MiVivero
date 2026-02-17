78263721981b1af20c6d8e5c6688b70f1e571c5a
EmaTronic <emanuelrodriguezcorchea@gmail.com>
2026-02-16 21:46:16 -0300

    Mejoras en la visa

--- app/src/main/java/com/emanuel/mivivero/MainActivity.kt
+++ app/src/main/java/com/emanuel/mivivero/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.emanuel.mivivero
 
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
@@ -16,11 +17,42 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Configurar Toolbar como ActionBar
+        setSupportActionBar(binding.topAppBar)
+
+        // Obtener NavController
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.navHost) as NavHostFragment
 
         val navController = navHostFragment.navController
 
+        // Conectar BottomNavigation
         binding.bottomNav.setupWithNavController(navController)
+
+        // Click icono usuario
+        binding.btnUsuario.setOnClickListener {
+
+            val navController =
+                (supportFragmentManager.findFragmentById(R.id.navHost) as NavHostFragment)
+                    .navController
+
+            navController.navigate(
+                R.id.registroUsuarioFragment,
+                null,
+                androidx.navigation.NavOptions.Builder()
+                    .setLaunchSingleTop(true)
+                    .build()
+            )
+        }
+
+
+        // Click logo (Acerca de)
+        binding.imgLogoToolbar.setOnClickListener {
+            AlertDialog.Builder(this)
+                .setTitle("Mi Vivero")
+                .setMessage("Versión 1.0\nGestión profesional de plantas.")
+                .setPositiveButton("Cerrar", null)
+                .show()
+        }
     }
 }
